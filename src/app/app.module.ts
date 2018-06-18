d9db3abb81259c75162dc8080b1d11e283c3236e
Wezck <wezck@mail.com>
2018-06-18 12:11:47 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
+
 // Routes
 import { APP_ROUTES } from './app.routes';
 
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { SignupComponent } from './signup/signup.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    Graficas1Component,
-    ProgressComponent,
-    BreadcrumbsComponent,
-    HeaderComponent,
-    NopagefoundComponent,
-    SidebarComponent,
-    SignupComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
